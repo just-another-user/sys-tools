fd65d799751651ce3f0ef49d9ed03554b9e1f809
just-another-user <thisismyregularmail@gmail.com>
2016-11-15 01:47:05 +0200

    gpipdate v0.20:
    Changes:
      - ADDED: Quit button was added.
      - IMPROVED: Refactored a bit as I'm learning the ropes of Tkinter.

--- gpipdate.py
+++ gpipdate.py
@@ -16,43 +16,24 @@ except:
 from pipdate import *
 
 
-__version__ = '0.1b'
-__last_updated__ = "05/11/2016"
+__version__ = '0.20'
+__last_updated__ = "15/11/2016"
 __author__ = 'just-another-user'
 
 
 # noinspection PyAttributeOutsideInit,PyMissingConstructor,PyUnusedLocal
 class PipdateGui(Frame):
 
-    def __init__(self, parent, **kw):
-        Frame.__init__(self, parent)
-
+    def __init__(self, parent, **kwargs):
+        Frame.__init__(self, parent, **kwargs)
         self.parent = parent
-
-        self.message_board = Label(self.parent)
-
-        self.init_ui()
-
-    def update_message_board(self, event=None, msg=None):
-        if msg is None:
-            self.message_board.configure(text="{} package(s) selected".format(
-                len(self.outdated_packages_listbox.curselection())))
-        else:
-            self.message_board.configure(text=str(msg))
+        self.parent.title("Pipdate GUI v{}".format(__version__))
+        self.available_pips = get_pip_paths()
+        if self.available_pips:
+            self.init_ui()
 
     def init_ui(self):
-        self.parent.title("Pipdate GUI v{}".format(__version__))
-
-        # Setup main frame.
-        mainframe = Frame(self.parent)
-        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-        mainframe.columnconfigure(0, weight=1)
-        mainframe.rowconfigure(0, weight=2)
-
         # Add "Available Pips" drop down list.
-        self.available_pips = get_pip_paths()
-        if not self.available_pips:
-            return False    # No pips found in the system. Nothing to do...     # TODO Change this...
         self.selected_pip = StringVar(value=self.available_pips[-1])
         self.available_pips_drop_down_list = OptionMenu(self.parent, self.selected_pip,
                                                         self.selected_pip.get(), *self.available_pips)
@@ -63,25 +44,35 @@ class PipdateGui(Frame):
         self.outdated_packages_listbox = Listbox(self.parent, selectmode='multiple')
         for item in list_outdated_packages(self.selected_pip.get()):
             self.outdated_packages_listbox.insert(END, item)
-        self.outdated_packages_listbox.grid(column=0, row=1, sticky=(W, E))
+        self.outdated_packages_listbox.grid(column=0, row=1, sticky=(W, E), rowspan=3)
 
         # Add buttons
-        select_all_packages_button = Button(self.parent, text="Select All Packages",
-                                            command=self.select_all_packages)
-        select_all_packages_button.grid(column=1, row=1, sticky=(N, W))
-        clear_selections_button = Button(self.parent, text="Clear All Selections",
-                                         command=self.clear_selections)
-        clear_selections_button.grid(column=1, row=1, sticky=W)
+        select_all_packages_button = Button(self.parent, text="Select All Packages", command=self.select_all_packages)
+        select_all_packages_button.grid(column=1, row=1, sticky=NW)
+
+        clear_selections_button = Button(self.parent, text="Clear All Selections", command=self.clear_selections)
+        clear_selections_button.grid(column=1, row=1, sticky=NW)
+
         updated_selected_button = Button(self.parent, text="Update Selected Packages",
                                          command=self.update_selected_packages)
-        updated_selected_button.grid(column=1, row=1, sticky=S)
+        updated_selected_button.grid(column=1, row=1, sticky=W)
 
-        self.message_board.grid(column=0, row=2, sticky=(N, W))
+        quit_button = Button(self.parent, text="Quit", command=sys.exit)
+        quit_button.grid(column=1, row=1, sticky=SW)
+
+        # Add 'message board' on which messages to the user will be displayed.
+        self.message_board_text = StringVar()
+        self.message_board = Label(self.parent, textvariable=self.message_board_text)
+        self.message_board.grid(column=0, row=4, sticky=(N, W), columnspan=2, pady=5)
 
         self.outdated_packages_listbox.bind("<<ListboxSelect>>", self.update_message_board)
 
-        # for child in mainframe.winfo_children():
-        #     child.grid_configure(padx=5, pady=5)
+    def update_message_board(self, event=None, msg=None):
+        if msg is None:
+            self.message_board_text.set("{} package(s) selected".format(
+                len(self.outdated_packages_listbox.curselection())))
+        else:
+            self.message_board_text.set(str(msg))
 
     def notify(self, msg=None):
         if msg is None:
@@ -95,7 +86,6 @@ class PipdateGui(Frame):
     def clear_selections(self):
         self.outdated_packages_listbox.selection_clear(0, END)
         self.update_message_board()
-        # self.notify(msg="Chosen pip is {}".format(self.available_pips_drop_down_list.selection_get()))
 
     def update_pip_outdated_listbox(self, *args):
         self.outdated_packages_listbox.destroy()
@@ -120,12 +110,12 @@ class PipdateGui(Frame):
             self.update_message_board(msg="Updating {}".format(packages_names))
             update_successful = batch_update_packages(self.selected_pip.get(), selected_packages_printable_format)
             if update_successful:
-                self.notify(msg="Packages updated successfully.")
+                self.notify(msg="Package(s) updated successfully.")
             else:
                 self.notify(msg="Some packages failed to update.")
             self.update_pip_outdated_listbox()
         else:
-            self.update_message_board(msg="No packages selected.")
+            self.update_message_board(msg="No package selected.")
 
 
 def main():

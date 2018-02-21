1d7b3f3289c302423bf2858e0c71cb2498a3eb44
just-another-user <thisismyregularmail@gmail.com>
2018-02-21 23:48:54 +0200

    install_tools v0.11:
    Changes:
      - FIXED: 'sudo' mistype

--- install_tools.sh
+++ install_tools.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # ############################################# #
 #
-# install_tools.sh (v0.1)
+# install_tools.sh (v0.11)
 # by just-another-user
 #
 # Description:
@@ -16,11 +16,11 @@ echo Installing base packages...
 sudo apt install -y python python3 python-dev python3-dev python-pip python3-pip python-setuptools python3-setuptools python-tk python3-tk build-essential libffi-dev openssl libssl-dev libsqlite3-dev libmysqlclient-dev git
 
 sudo ln -snf `pwd`/aptdate /usr/local/bin/aptdate
-sydi chmod +x /usr/local/bin/aptdate
+sudo chmod +x /usr/local/bin/aptdate
 sudo echo `which python3` `pwd`/pipdate.py \$\* > /usr/local/bin/pipdate
-sydi chmod +x /usr/local/bin/pipdate
+sudo chmod +x /usr/local/bin/pipdate
 sudo ln -snf `pwd`/where /usr/local/bin/where
-sydi chmod +x /usr/local/bin/where
+sudo chmod +x /usr/local/bin/where
 
 echo Updating Python packages...
 pipdate
